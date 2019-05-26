3a15c8f062a589b7ec41d5b649c78b1231d7f4e0
15562592216 <729048330@qq.com>
2019-05-26 10:52:34 +0800

    2019-05-26 上午

---
+++ lib/model/post.dart
@@ -0,0 +1,34 @@
+class Post {
+  const Post({
+    this.title,
+    this.author,
+    this.imageUrl
+  });
+
+  final String title;
+  final String author;
+  final imageUrl;
+}
+
+final List<Post> posts = [
+  Post(
+    title: "title1",
+    author: "Reloaded 1 of 440 libraries",
+    imageUrl: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3300305952,1328708913&fm=27&gp=0.jpg",
+  ),
+  Post(
+    title: "title12",
+    author: "Reloaded 1 of 440 libraries",
+    imageUrl: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3300305952,1328708913&fm=27&gp=0.jpg",
+  ),
+  Post(
+    title: "title13",
+    author: "Reloaded 1 of 440 libraries",
+    imageUrl: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3300305952,1328708913&fm=27&gp=0.jpg",
+  ),
+  Post(
+    title: "title14",
+    author: "Reloaded 1 of 440 libraries",
+    imageUrl: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3300305952,1328708913&fm=27&gp=0.jpg",
+  ),
+];

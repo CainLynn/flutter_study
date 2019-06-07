0517948ebad7ebd5c170b31b68670feef107440e
cain <cain@caindeMacBook-Pro.local>
2019-06-07 22:05:01 +0800

    ....

--- lib/demo/from_demo.dart
+++ lib/demo/from_demo.dart
@@ -9,8 +9,38 @@ class FromDemo extends StatelessWidget {
         data: Theme.of(context).copyWith(
           primaryColor: Colors.black
         ),
-        child: ThemeDemo(),
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFieldDemo()
+            ],
+          ),
+        ),
       )
+    );
+  }
+}
+
+class TextFieldDemo extends StatefulWidget{
+  TextFieldDemoState createState() => TextFieldDemoState();
+}
+  
+class TextFieldDemoState extends State<TextFieldDemo> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return TextField(
+      decoration: InputDecoration(
+        icon: Icon(Icons.text_fields),
+        labelText: "Title",
+        hintText: "Enter the post title.",
+        // border: InputBorder.none,
+        border: OutlineInputBorder(),
+        filled: true,
+        fillColor: Colors.red[100]
+      ),
     );
   }
 }

9286ff139774f3e7579968bf714f541452a90225
cain <cain@caindeMacBook-Pro.local>
2019-06-08 11:50:52 +0800

    表单

--- lib/demo/from_demo.dart
+++ lib/demo/from_demo.dart
@@ -14,7 +14,8 @@ class FromDemo extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              TextFieldDemo()
+              // TextFieldDemo(),
+              Register()
             ],
           ),
         ),
@@ -23,15 +24,142 @@ class FromDemo extends StatelessWidget {
   }
 }
 
+class Register extends StatefulWidget {
+  Register({Key key}) : super(key: key);
+
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+
+  final registerFormKey = GlobalKey<FormState>();
+  String username,password;
+  bool autovalidate = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Form(
+         key: registerFormKey,
+         child: Column(
+           children: <Widget>[
+             TextFormField(
+               decoration: InputDecoration(
+                 labelText: "Username",
+                 helperText: ""
+               ),
+               onSaved: (value) {
+                 username = value;
+               },
+               validator: validateUsername,
+               autovalidate: autovalidate,
+             ),
+             TextFormField(
+               obscureText: true,
+               decoration: InputDecoration(
+                 labelText: "Password",
+                 helperText: "",
+               ),
+               onSaved: (value) {
+                 password = value;
+               },
+               validator: validatePassword,
+               autovalidate: autovalidate,
+             ),
+             SizedBox(
+               height: 10,
+             ),
+             Container(
+               width: double.infinity,
+               child: RaisedButton(
+                 color: Theme.of(context).accentColor,
+                 child: Text("Register", style: TextStyle(color: Colors.white)),
+                 elevation: 0,
+                 onPressed: submitRegisterForm,
+               )
+             )
+           ],
+         ),
+       ),
+    );
+  }
+
+  submitRegisterForm() {
+    if (registerFormKey.currentState.validate()) {
+
+      registerFormKey.currentState.save();
+      
+      debugPrint("username: $username");
+      debugPrint("password: $password");
+
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Registering..."),
+        )
+      );
+    }
+    else {
+      autovalidate = true;
+    }
+  }
+
+  String validateUsername(String value) {
+
+    if (value.isNotEmpty) {
+
+      return null;
+    }
+
+    return "Username is empty";
+  }
+
+  String validatePassword(String value) {
+
+  if (value.isNotEmpty) {
+
+      return null;
+    }
+
+    return "Password is empty";
+  }
+}
+
 class TextFieldDemo extends StatefulWidget{
   TextFieldDemoState createState() => TextFieldDemoState();
 }
   
 class TextFieldDemoState extends State<TextFieldDemo> {
+
+  final _textEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() { 
+    super.initState();
+    _textEditingController.text = "hi";
+    _textEditingController.addListener(
+      () {
+        debugPrint("input: ${_textEditingController.text}");
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return TextField(
+      controller: _textEditingController,
+      // onChanged:(value) {
+      //   debugPrint("input: $value");
+      // },
+      onSubmitted: (value) {
+        debugPrint("submit: $value");
+      },
       decoration: InputDecoration(
         icon: Icon(Icons.text_fields),
         labelText: "Title",
